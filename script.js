0e32d15022791094790db5c55bfc03c98e71d0dd
Aluno <aluno@naverecife.local>
2022-09-21 21:33:26 -0300

    Quase final

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ btn.textContent = "+";
 btn1.textContent = "-";
 btn2.textContent = "/";
 btn3.textContent = "x";
-input.type = "number"
-input1.type = "number"
-input.placeholder = "Número1"
+input.type = "number";
+input1.type = "number";
+input.placeholder = "Número1";
+input1.placeholder = "Número2";
 
 const div = document.createElement("div");
 const div1 = document.createElement("div");
-const div2 = document.createElement("div");
 
 div.appendChild(input);
 div.appendChild(input1);
@@ -25,13 +25,45 @@ div1.appendChild(btn);
 div1.appendChild(btn1);
 div1.appendChild(btn2);
 div1.appendChild(btn3);
-div2.appendChild(p);
-document.body.appendChild(div2);
+document.body.appendChild(p);
 document.body.appendChild(div);
 document.body.appendChild(div1);
 
 // Css
+// body
+document.body.style.backgroundColor = "aqua";
 document.body.style.display = "flex";
+document.body.style.flexDirection = "column";
+document.body.style.justifyContent = "center";
+document.body.style.alignItems = "center";
+document.body.style.height = "90vh";
+// Div
+div.style.display = "flex";
+div.style.flexDirection = "column";
+div1.style.marginTop = "5px";
+
+
+// P
+p.style.backgroundColor = "white";
+p.style.border = "1px solid black"
+p.style.width = "175px"
+p.style.color = "gray";
+p.style.textAlign = "center";
+p.style.borderRadius = "10px";
+
+// input
+input.style.marginBottom = "5px";
+input.style.borderRadius = "10px";
+input1.style.borderRadius = "10px";
+// btn
+btn.style.borderRadius = "50%";
+btn.style.border = "1px solid gray";
+btn1.style.borderRadius = "50%";
+btn1.style.border = "1px solid gray";
+btn2.style.borderRadius = "50%";
+btn2.style.border = "1px solid gray";
+btn3.style.borderRadius = "50%";
+btn3.style.border = "1px solid gray";
 
 // /javaScript
 let resultado;
